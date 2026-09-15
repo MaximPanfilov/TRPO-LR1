eaa1e5d99c86c47ee0e9ac4fa7af74f967f3ceae
MaximPanfilov <makspanfilov929@gmail.com>
2026-09-15 22:02:31 +0300

    [*] New commentaries for logger

---
+++ logger.h
@@ -0,0 +1,10 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <string>
+
+void logAction(const std::string& action);
+void logError(const std::string& error);
+//[TODO] Add new interfaces here mb?
+//You mean right here?
+#endif
